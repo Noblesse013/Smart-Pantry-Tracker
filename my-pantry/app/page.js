604b5fa3e8032fcca65678656fdf1e82a3f758d4
Noblesse013 <mehreen.mallick.fiona@.gbracu.ac.bd>
2024-07-30 13:57:32 +0600

    hello fio

--- my-pantry/app/page.js
+++ my-pantry/app/page.js
@@ -1,7 +1,14 @@
 
 export default function Home() {
   return (
-   
+   <box
+    height="120vh"
+    width="100vw"
+    display={"flex"}
+    justify-content={"center"}
+    align-items={"center"}
+    >Hello Fio
+   </box>
 
 
   );
