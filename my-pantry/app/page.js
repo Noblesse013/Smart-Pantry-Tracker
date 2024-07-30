51e13129c03a6da809c1bb29c0e6040043f82ad7
Noblesse013 <mehreen.mallick.fiona@.gbracu.ac.bd>
2024-07-30 14:35:58 +0600

    om

--- my-pantry/app/page.js
+++ my-pantry/app/page.js
@@ -1,15 +1,25 @@
+import{ Box, Stack } from "@mui/material"
 
+const stuff=['carrot']
 export default function Home() {
   return (
-   <box
-    height="120vh"
-    width="100vw"
-    display={"flex"}
-    justify-content={"center"}
-    align-items={"center"}
-    >Hello Fio
-   </box>
+   <Box
+      height="120vh"
+      width="100vw"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+
+      <Stack width="900px" height="1100px" spacing={2}>
+
+        {stuff.map((item) => (
+          <Box
+      
+          
+      </Stack>
+    </Box>
 
 
-  );
+  )
 }
