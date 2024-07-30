c3c7f145eb2547bf44e54e79f2d0caac0c8e419f
Noblesse013 <mehreen.mallick.fiona@.gbracu.ac.bd>
2024-07-30 22:43:13 +0600

    ok

--- my-pantry/app/page.js
+++ my-pantry/app/page.js
@@ -1,6 +1,6 @@
-import{ Box, Stack } from "@mui/material"
+import{ Box, Stack, Typography } from "@mui/material"
 
-const stuff=['carrot']
+const item=['carrot','tomato','green-chili','brinjal','ladies finger','potato','onion','cucumber','honey',]
 export default function Home() {
   return (
    <Box
@@ -8,16 +8,44 @@ export default function Home() {
       width="100vw"
       display={"flex"}
       justifyContent={"center"}
+      flexDirection={"column"}
       alignItems={"center"}
     >
+    <Box marginBottom={1} width="500px" height="100px" bgcolor={"lightblue"}  border={"20px"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+    <Typography 
+              variant={"h4"}
+              color={"White"}
+              textAlign={"center"}
+              fontWeight={"bold"}>
+      Pantry Tracker
+     </Typography>
+   
+      </Box> 
+      <Stack width="500px" height="350px" spacing={1} overflow={'auto'} border={'1px solid grey'}>
 
-      <Stack width="900px" height="1100px" spacing={2}>
-
-        {stuff.map((item) => (
+        {item.map((i) => (
           <Box
-      
-          
-      </Stack>
+            key={item}
+            width="100%"
+            height="100px"
+            bgcolor={"lightpink"}
+            borderRadius={"5px 5px 10px 10px"}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Typography 
+              variant={"h5"}
+              color={"white"}
+              textAlign={"center"}
+              fontWeight={"bold"}
+        
+            
+            > 
+              {i.charAt(0).toUpperCase() + i.slice(1)}
+            </Typography>
+          </Box>  
+        ))}</Stack>
     </Box>
 
 
