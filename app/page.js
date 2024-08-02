afbc30a80bac67d7f5e9d1cb9d88ce0cf9f453e6
Noblesse013 <mehreen.mallick.fiona@.gbracu.ac.bd>
2024-08-02 23:32:56 +0600

    ok

--- app/page.js
+++ app/page.js
@@ -1,282 +1,274 @@
-"use client";
-import { Box, Stack, Typography, Button, TextField, Modal } from "@mui/material";
-import { firestore } from "@/firebase";
-import { collection, getDocs, query, doc, setDoc, getDoc, updateDoc, deleteDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
+'use client'
+import { useState, useEffect } from 'react';
+import { firestore } from '@/firebase';
+import { Box, Modal, Typography, Stack, TextField, Button, Paper, List, ListItem, ListItemText } from '@mui/material';
+import { collection, getDocs, query, setDoc, getDoc, deleteDoc, doc } from 'firebase/firestore';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Fastfood } from '@mui/icons-material'; // Import the food icon
+import axios from 'axios';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '100%',
-  maxWidth: 400,
-  bgcolor: '#FFC0CB',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 3,
-  borderRadius: '8px',
-};
-
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+// Replace this with your actual Spoonacular API key
+const SPOONACULAR_API_KEY = '4f9ffa37adb0463da6fcc38482a9710f';
 
 export default function Home() {
-  const [pantry, setPantryList] = useState([]);
+  const [inventory, setInventory] = useState([]);
+  const [filteredInventory, setFilteredInventory] = useState([]);
   const [open, setOpen] = useState(false);
-  const [itemName, setItemName] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredPantry, setFilteredPantry] = useState([]);
-  const [isClient, setIsClient] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const [itemQuantity, setItemQuantity] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [currentItem, setCurrentItem] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [recipesOpen, setRecipesOpen] = useState(false);
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
 
-  const updatePantry = async () => {
-    const q = query(collection(firestore, "pantry"));
-    const querySnapshot = await getDocs(q);
-    const pantryList = [];
-    querySnapshot.forEach((doc) => {
-      pantryList.push({ name: doc.id, count: doc.data().count });
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data()
+      });
     });
-    setPantryList(pantryList);
-    setFilteredPantry(pantryList);
-  };
 
-  useEffect(() => {
-    setIsClient(true);
-    updatePantry();
-  }, []);
-
-  const addItem = async (item) => {
-    try {
-      const normalizedItem = item.toLowerCase();
-      const docRef = doc(collection(firestore, 'pantry'), normalizedItem);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        await updateDoc(docRef, {
-          count: docSnap.data().count + 1
-        });
-      } else {
-        await setDoc(docRef, { count: 1 });
-      }
-
-      updatePantry();
-      handleClose();
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
+    setInventory(inventoryList);
+    setFilteredInventory(inventoryList);
   };
 
   const removeItem = async (item) => {
-    try {
-      const docRef = doc(collection(firestore, 'pantry'), item.name.toLowerCase());
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const currentCount = docSnap.data().count;
-        if (currentCount > 1) {
-          await updateDoc(docRef, {
-            count: currentCount - 1
-          });
-        } else {
-          await deleteDoc(docRef);
-        }
-      }
-
-      updatePantry();
-    } catch (error) {
-      console.error("Error removing document: ", error);
+    if (!item) {
+      console.error('Invalid item:', item);
+      return;
     }
+
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    await deleteDoc(docRef);
+    await updateInventory();
   };
+
+  const addItem = async (item, quantity) => {
+    if (!item) {
+      console.error('Invalid item:', item);
+      return;
+    }
+
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      await setDoc(docRef, { quantity: parseInt(quantity) });
+    } else {
+      await setDoc(docRef, { quantity: parseInt(quantity) });
+    }
+    await updateInventory();
+  };
+
+  const handleOpen = (item = '', quantity = 1) => {
+    setCurrentItem(item);
+    setItemName(item);
+    setItemQuantity(quantity);
+    setIsUpdate(!!item);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setItemName('');
+    setItemQuantity(1);
+    setIsUpdate(false);
+    setCurrentItem('');
+  };
+
+  const handleRecipesOpen = () => {
+    setRecipesOpen(true);
+  };
+
+  const handleRecipesClose = () => {
+    setRecipesOpen(false);
+  };
+
+  const fetchRecipes = async () => {
+    const ingredients = inventory.map(item => item.name).join(', ');
+    const response = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&apiKey=${SPOONACULAR_API_KEY}`);
+    setRecipes(response.data);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
 
   const handleSearch = (query) => {
     setSearchQuery(query);
-    if (query === "") {
-      setFilteredPantry(pantry);
+    if (query === '') {
+      setFilteredInventory(inventory);
     } else {
-      const filtered = pantry.filter(item => item.name.includes(query.toLowerCase()));
-      setFilteredPantry(filtered);
+      setFilteredInventory(inventory.filter(item => item.name.toLowerCase().includes(query.toLowerCase())));
     }
   };
 
-  const handleAddItem = (e) => {
-    e.preventDefault();
-    if (itemName.trim() !== "") {
-      addItem(itemName);
-      setItemName("");
-    }
-  };
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#8e24aa', // Violet color
+      },
+      secondary: {
+        main: '#ba68c8', // Lighter violet color
+      },
+      background: {
+        default: '#f3e5f5',
+      },
+    },
+  });
 
-  if (!isClient) {
-    return null;
-  }
+  const backgroundImageUrl = 'https://source.unsplash.com/featured/?pantry'; // URL for pantry-themed background
 
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display={'flex'}
-      justifyContent={'center'}
-      flexDirection={'column'}
-      alignItems={'center'}
-      gap={2}
-      bgcolor={'#FFEBEF'}
-      p={2}
-      pt={16}
-      pb={18}
-    >
-      <Box 
-        display="flex" 
-        flexDirection="column" 
-        alignItems="center" 
-        gap={2} 
-        width="100%" 
-        maxWidth="600px"
-        mb={2}
-      >
-        <TextField
-          id="search"
-          label="Looking for something?"
-          variant="outlined"
-          value={searchQuery}
-          onChange={(e) => handleSearch(e.target.value)}
-          fullWidth
-          sx={{ bgcolor: '#fff', borderRadius: '14px' }}
-        />
-        <Button 
-          variant="contained" 
-          onClick={handleOpen} 
-          sx={{ 
-            bgcolor: '#FF69B4', 
-            '&:hover': { bgcolor: '#FF1493' },
-            color: '#fff',
-            fontWeight: 'bold',
-            borderRadius: '8px',
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-            fontSize: '0.75rem',
-            padding: '4px 8px',
-            mt: 0.5
-          }}
-        >
-          Add New Items
-        </Button>
-      </Box>
-      <Box 
-        width="100%" 
-        maxWidth="500px" 
- 
-        boxShadow={'0 4px 8px rgba(0, 0, 0, 0.1)'} 
-        mt={2}
+    <ThemeProvider theme={theme}>
+      <Box
+        width="100vw"
+        height="100vh"
         display="flex"
         flexDirection="column"
+        justifyContent="center"
         alignItems="center"
+        gap={4}
+        p={3}
+        sx={{ 
+          backgroundImage: `url(${backgroundImageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundColor: theme.palette.background.default 
+        }}
       >
-        <Box
-          width="100%"
-          height="60px"
-          bgcolor={'#FFB6C1'}
-          display={'flex'}
-          justifyContent={'center'}
-          alignItems={'center'}
-          borderBottom={'1px solid #333'}
-          sx={{ borderRadius: '6px 6px 0 0' }}
-        >
-          <Typography variant={'h5'} color={'#333'} textAlign={'center'} fontWeight={'bold'} >
-            Pantry Tracker
+        <Box display="flex" alignItems="center" mb={2}>
+          <Fastfood fontSize="large" color="primary" sx={{ mr: 1 }} /> {/* Food icon */}
+          <Typography variant="h3" color="primary">
+            Your Pantry Tracker
           </Typography>
         </Box>
-        <Stack
-          width="100%"
-          height="300px"
-          spacing={2}
-          overflowY={'auto'}
-          padding={2}
-        >
-          {filteredPantry.length > 0 ? filteredPantry.map((item) => (
-            <Box
-              key={item.name}
-              width="100%"
-              display={'flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}
-              bgcolor={'#f8f8f8'}
-              border={'1px solid #ccc'}
-              borderRadius={4}
-              p={2}
-              boxShadow={'0 2px 4px rgba(0, 0, 0, 0.1)'}
-              maxWidth="90%"
-              mx="auto"
-            >
-              <Box display={'flex'} flexDirection={'column'} justifyContent={'center'}>
-                <Typography variant={'h6'} color={'#333'} fontWeight={'bold'}>
-                  {capitalizeFirstLetter(item.name)}
-                </Typography>
-                <Typography variant={'body2'} color={'#666'}>
-                  Quantity: {item.count}
-                </Typography>
-              </Box>
-              <Button 
-                variant="contained" 
-                color="secondary" 
-                onClick={() => removeItem(item)} 
-                sx={{
-                  bgcolor: '#FFB6C1',
-                  '&:hover': { bgcolor: '#FF69B4' },
-                  padding: '4px 8px',
-                  fontSize: '0.875rem',
-                  color: '#fff',
-                  fontWeight: 'bold',
-                  borderRadius: '8px',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        <Stack direction="row" spacing={2} mb={2} width="100%" maxWidth="800px">
+          <TextField
+            variant="outlined"
+            fullWidth
+            placeholder="Search for an item"
+            value={searchQuery}
+            onChange={(e) => handleSearch(e.target.value)}
+          />
+          <Button variant="contained" color="primary" onClick={() => handleOpen()}>
+            Add Item
+          </Button>
+          <Button variant="contained" color="secondary" onClick={() => {
+            fetchRecipes();
+            handleRecipesOpen();
+          }}>
+            Get Recipes
+          </Button>
+        </Stack>
+        <Paper elevation={3} sx={{ width: '100%', maxWidth: '800px', p: 3 }}>
+          <Stack spacing={2}>
+            {filteredInventory.length === 0 ? (
+              <Typography variant="h6" color="textSecondary" align="center">
+                No items found.
+              </Typography>
+            ) : (
+              filteredInventory.map(({ name, quantity }) => (
+                <Paper key={name} sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Typography variant="h6">{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+                  <Typography variant="h6">{quantity}</Typography>
+                  <Box>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => handleOpen(name, quantity)}
+                      sx={{ mr: 1 }}
+                    >
+                      Update
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      color="secondary"
+                      onClick={() => removeItem(name)}
+                    >
+                      Delete
+                    </Button>
+                  </Box>
+                </Paper>
+              ))
+            )}
+          </Stack>
+        </Paper>
+        <Modal open={open} onClose={handleClose}>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            sx={{ transform: "translate(-50%, -50%)", width: 400, bgcolor: "background.paper", borderRadius: 1, boxShadow: 24, p: 4 }}
+          >
+            <Typography variant="h6" color="textPrimary" mb={2}>
+              {isUpdate ? 'Update Item' : 'Add Item'}
+            </Typography>
+            <Stack spacing={2}>
+              <TextField
+                variant='outlined'
+                fullWidth
+                label="Item Name"
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+              <TextField
+                variant='outlined'
+                fullWidth
+                type="number"
+                label="Quantity"
+                value={itemQuantity}
+                onChange={(e) => setItemQuantity(e.target.value)}
+              />
+              <Button
+                variant='contained'
+                color="primary"
+                onClick={() => {
+                  addItem(itemName, itemQuantity);
+                  handleClose();
                 }}
               >
-                Remove
+                {isUpdate ? 'Update' : 'Add'}
               </Button>
-            </Box>
-          )) : (
-            <Typography variant={'h6'} color={'#333'} textAlign={'center'} fontWeight={'bold'}>
-              Your Pantry is Empty
-            </Typography>
-          )}
-        </Stack>
-      </Box>
-
-      <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
-          <Typography variant="h6" component="h2" textAlign="center">
-            Add Item
-          </Typography>
-          <TextField
-            label="Item Name"
-            variant="outlined"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-            fullWidth
-          />
-          <Button
-            variant="contained"
-            onClick={handleAddItem}
-            sx={{
-              bgcolor: '#FF69B4',
-              '&:hover': { bgcolor: '#FF1493' },
-              color: '#fff',
-              fontWeight: 'bold',
-              borderRadius: '8px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-              fontSize: '0.875rem',
-              padding: '8px 16px',
-            }}
+            </Stack>
+          </Box>
+        </Modal>
+        <Modal open={recipesOpen} onClose={handleRecipesClose}>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            sx={{ transform: "translate(-50%, -50%)", width: 600, bgcolor: "background.paper", borderRadius: 1, boxShadow: 24, p: 4 }}
           >
-            Add
-          </Button>
-        </Box>
-      </Modal>
-    </Box>
+            <Typography variant="h6" color="textPrimary" mb={2}>
+              Recipe Suggestions
+            </Typography>
+            <List>
+              {recipes.length === 0 ? (
+                <Typography variant="body1" color="textSecondary">
+                  No recipes found.
+                </Typography>
+              ) : (
+                recipes.map(recipe => (
+                  <ListItem key={recipe.id}>
+                    <ListItemText
+                      primary={recipe.title}
+                      secondary={`Used ingredients: ${recipe.usedIngredientCount}, Missed ingredients: ${recipe.missedIngredientCount}`}
+                    />
+                  </ListItem>
+                ))
+              )}
+            </List>
+          </Box>
+        </Modal>
+      </Box>
+    </ThemeProvider>
   );
 }
+
